567e27566ca201f0c0e0fff6034470cee79ca20d
Krol22 <krol22ee@gmail.com>
2017-10-28 21:16:29 +0200

    Removing unnecessary files

--- js/services/component.js
+++
@@ -1,7 +0,0 @@
-const Component = function(config) {
-    this.content = config.content;
-    this.script = config.script;
-    this.selector = config.selector;
-
-    System.addComponent(this);
-};
